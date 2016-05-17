e7f6bc127b3092ecb42902eefb87c7a1e8591f7c
Jason Martocci <martoccijason@gmail.com>
2016-05-17 12:12:30 -0400

    started the wireframming

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,39 +3,19 @@ var router = express.Router();
 var burgers = require('../model/burger.js');
 
 router.get('/', function(req,res) {
-	res.redirect('/burger')
+		res.render('index');
 });
 
-router.get('/burger', function(req,res) {
-	burgers.all(function(data){
-		var hbsObject = {burgers : data}
-		console.log(hbsObject)
-		res.render('index', hbsObject);
-	});
+router.get('/inventory', function(req,res) {
+		res.render('inventory');
 });
 
-router.post('/burger/create', function(req,res) {
-	burgers.create(['burger_name'], [req.body.name], function(data){
-		res.redirect('/burger')
-	});
+router.get('/products', function(req,res) {
+		res.render('products');
 });
 
-router.put('/burger/update/:id', function(req,res) {
-	var condition = 'id = ' + req.params.id;
-
-	console.log(req.body.devoured);
-
-	burgers.update({'devoured' : req.body.devoured}, condition, function(data){
-		res.redirect('/burger');
-	});
+router.get('/sign_in', function(req,res) {
+		res.render('sign_in');
 });
 
-router.delete('/burger/delete/:id', function(req,res) {
-	var condition = 'id = ' + req.params.id;
-
-	burgers.delete(condition, function(data){
-		console.log(data);
-		res.redirect('/burger');
-	});
-});
 module.exports = router;
