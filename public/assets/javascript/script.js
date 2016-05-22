2ed18ffbce8f10af9fbc73672168acaa9496b1c1
williamavasquez <william.a.vasquez@gmail.com>
2016-05-22 16:57:22 -0400

    feat [barcode] added // ajax call added to inventory

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -3,14 +3,21 @@ $(document).ready(function() {
     $('#shop').DataTable();
 
 	$("#register").hide();
-	
+
 	$("#register_btn").click(function(){
 	    $("#sign_in").hide();
 	    $("#register").show();
 	});
-	
+
 	$("#signIn_btn").click(function(){
 	    $("#sign_in").show();
 	    $("#register").hide();
 	});
-} );
+
+  $("#scan").click(function(){
+    $("#barcodeScannerWindow").show()
+    $.getScript("/assets/javascript/barcodescan.js",function(){
+        quagga()
+      }); //$.getScript
+  });
+}); //$(document).ready(function()
